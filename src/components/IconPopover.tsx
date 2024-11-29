0e5777d6604d3e6d4abf77debdc290c09164e796
Monica Carvajal <monik.182@gmail.com>
2024-11-29 01:48:31 +0100

    fix icon popover bug

--- src/components/IconPopover.tsx
+++ src/components/IconPopover.tsx
@@ -3,58 +3,57 @@ import { ExtendedComponentProps, ListProps, NoteProps } from '../types'
 import { SlAnchor, SlBasket, SlBriefcase, SlCalender, SlCreditCard, SlCup, SlDiamond, SlDirections, SlEarphones, SlEmotsmile, SlEnergy, SlEnvolope, SlEyeglass, SlFeed, SlFilm, SlFire, SlGameController, SlGhost, SlGlobeAlt, SlGraduation, SlHandbag, SlHeart, SlHome, SlLike, SlMagicWand, SlMusicToneAlt, SlMustache, SlNotebook, SlPhone, SlPlane, SlPresent, SlQuestion, SlRocket, SlStar, SlTarget, SlTrophy, SlWallet, SlWrench } from "react-icons/sl"
 import { PopoverArrow, PopoverBody, PopoverContent, PopoverRoot, PopoverTitle, PopoverTrigger } from './ui/popover'
 import { IconButton } from '@chakra-ui/react'
-import * as SimpleLineIcons from 'react-icons/sl'
 
-const icons = [
-  <SlBasket />,
-  <SlCup />,
-  <SlDiamond />,
-  <SlEnergy />,
-  <SlEnvolope />,
-  <SlGameController />,
-  <SlCreditCard />,
-  <SlDirections />,
-  <SlEarphones />,
-  <SlEmotsmile />,
-  <SlEyeglass />,
-  <SlFilm />,
-  <SlFeed />,
-  <SlHandbag />,
-  <SlHeart />,
-  <SlHome />,
-  <SlLike />,
-  <SlGlobeAlt />,
-  <SlGraduation />,
-  <SlMusicToneAlt />,
-  <SlPlane />,
-  <SlPresent />,
-  <SlStar />,
-  <SlRocket />,
-  <SlWallet />,
-  <SlWrench />,
-  <SlHome />,
-  <SlMagicWand />,
-  <SlPhone />,
-  <SlNotebook />,
-  <SlFire />,
-  <SlGhost />,
-  <SlMustache />,
-  <SlQuestion />,
-  <SlTrophy />,
-  <SlTarget />,
-  <SlCalender />,
-  <SlAnchor />,
-  <SlBriefcase />,
-]
+const iconMapping = {
+  SlBasket: <SlBasket />,
+  SlCup: <SlCup />,
+  SlDiamond: <SlDiamond />,
+  SlEnergy: <SlEnergy />,
+  SlEnvolope: <SlEnvolope />,
+  SlGameController: <SlGameController />,
+  SlCreditCard: <SlCreditCard />,
+  SlDirections: <SlDirections />,
+  SlEarphones: <SlEarphones />,
+  SlEmotsmile: <SlEmotsmile />,
+  SlEyeglass: <SlEyeglass />,
+  SlFilm: <SlFilm />,
+  SlFeed: <SlFeed />,
+  SlHandbag: <SlHandbag />,
+  SlHeart: <SlHeart />,
+  SlHome: <SlHome />,
+  SlLike: <SlLike />,
+  SlGlobeAlt: <SlGlobeAlt />,
+  SlGraduation: <SlGraduation />,
+  SlMusicToneAlt: <SlMusicToneAlt />,
+  SlPlane: <SlPlane />,
+  SlPresent: <SlPresent />,
+  SlStar: <SlStar />,
+  SlRocket: <SlRocket />,
+  SlWallet: <SlWallet />,
+  SlWrench: <SlWrench />,
+  SlMagicWand: <SlMagicWand />,
+  SlPhone: <SlPhone />,
+  SlNotebook: <SlNotebook />,
+  SlFire: <SlFire />,
+  SlGhost: <SlGhost />,
+  SlMustache: <SlMustache />,
+  SlQuestion: <SlQuestion />,
+  SlTrophy: <SlTrophy />,
+  SlTarget: <SlTarget />,
+  SlCalender: <SlCalender />,
+  SlAnchor: <SlAnchor />,
+  SlBriefcase: <SlBriefcase />,
+}
 export function IconPopover({ icon = 'SlMagicWand', onChange }: ExtendedComponentProps<Partial<ListProps | NoteProps>>) {
   const [Icon, setIcon] = useState<string>(icon)
   const [open, setOpen] = useState(false)
+  const IconComponent = Icon ? (iconMapping as any)[Icon] : null
+
   const handleSelect = (icon: string) => {
     setOpen(false)
     setIcon(icon)
     onChange?.({icon})
   }
-  const IconComponent = (SimpleLineIcons as any)[Icon]
 
   return (
     <PopoverRoot open={open} onOpenChange={(e) => setOpen(e.open)}>
@@ -64,7 +63,7 @@ export function IconPopover({ icon = 'SlMagicWand', onChange }: ExtendedComponen
           variant="ghost"
           size="md"
         >
-          <IconComponent />
+          {IconComponent}
         </IconButton>
       </PopoverTrigger>
       <PopoverContent>
@@ -72,15 +71,15 @@ export function IconPopover({ icon = 'SlMagicWand', onChange }: ExtendedComponen
         <PopoverBody>
           <PopoverTitle fontWeight="medium">Select icon</PopoverTitle>
           <div>
-            {icons.map((icon, index) => (
+            {Object.keys(iconMapping).map((icon, index) => (
               <IconButton
                 key={index}
                 aria-label="Icon"
                 variant="ghost"
                 size="sm"
-                onClick={() => handleSelect(icon.type.name)}
+                onClick={() => handleSelect(icon)}
               >
-                {icon}
+                {(iconMapping as any)[icon]}
               </IconButton>
             ))}
           </div>
